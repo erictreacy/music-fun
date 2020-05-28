db58357d6aed3d3e46621241087d4f7a8f21cddb
Ubuntu <ubuntu@ip-172-31-57-178.ec2.internal>
2020-05-28 17:27:17 +0000

    migrate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+/*import './App.css'*/
+import Album from './Album.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import months from './months.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+        <h1>Top 10 Albums</h1>
+        <p>The music that influenced my youth by <a href="//etreacy.me">Eric Treacy</a></p>
+          <p>
+            One of my friends, <a href="https://www.facebook.com/ianmwalling">Ian Walling</a>, gave me the task of choosing the top 10 albums that greatly 
+            influenced my taste in music. One album a day, for 10 
+            consecutive days. No explanations, no reviews, just covers. I decided to do my own version.
+          </p>
+
+          <p>
+            Tap on a cover to stream it on Spotify, or {' '}
+            <a href="//etreacy.me"> read more</a>
+            {' '} about my youth's musical influence.
+          </p>
+
+          
+          <br/>
+          <br/>
+        </div>
+        {months.map(month => (
+          <Album month={month.month} url={month.url} cover={month.cover} />
+        ))}
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/months.js
@@ -0,0 +1,69 @@
+const months = [
+  {
+    month: "Nirvana's 'Nevermind' is the second studio album by American rock band Nirvana, released on September 24, 1991, by DGC Records. A number of labels courted the band, but Nirvana ultimately signed with Geffen Records imprint DGC Records based upon repeated recommendations from Kim Gordon of Sonic Youth and their management company. Produced by Butch Vig, it was the band's first release on the label, as well as the first to feature drummer Dave Grohl. The album cover, which shows a naked baby swimming with a U.S. dollar bill on a fishhook just out of his reach, is often recognized as one of the most famous album covers in popular music.",
+    cover: "nevermind_nirvana.jpg",
+    url: "https://open.spotify.com/album/2guirTSEqLizK7j9i1MTTZ?si=dya70LB6QrexDa3qWgJNag",
+  },
+  {
+    month: "The National's 'Boxer' is the fourth studio album by the American indie rock band, released on May 22, 2007 on Beggars Banquet Records. Produced by both Peter Katis and the band itself, the album has sold nearly 430,000 copies in the U.S. Following its release, the album debuted at #68 on the U.S. Billboard 200, selling about 9,500 copies in its first week.",
+    cover: "boxer_thenational.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/2MUG9i4c6ckIk0CwUgk748?si=D5FmDEy6QvC5RdGizS172w",
+  },
+  {
+    month: "Breathe Owl Breathe",
+    cover: "breathe_owl_breathe_magic_central.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/0tHPJ7XE6AkmUhtYMSpvXA?si=6bzznEurQtWoNq4yRM7Uvg",
+  },
+  {
+    month: "Cat Stevens",
+    cover: "cat_stevens_tea.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/471dRLRbUIj1tOn1gubhxA?si=yJ_K2-5pTry2QrR2BexUZg",
+  },
+  {
+    month: "Face To Face",
+    cover: "face_to_face.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/79n9NY7Jm7PB4KGNrrXccD?si=T3R8Iw5JRsaYeNiEPBS27Q",
+  },
+  {
+    month: "Arcade Fire",
+    cover: "funeral_arcadefire.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/7fbT1xQlvjZMj1V4CP07IQ?si=Si9aX3MnRz-sAeg1kFjzCQ",
+  },
+  {
+    month: "July",
+    cover: "2017-07.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/2fUMoxQlnWd1kUcT4z87ZG?si=TXJj3buzRMmEYZ-I3QKD5Q",
+  },
+  {
+    month: "August",
+    cover: "2017-08.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/3e2ABAA8u5Z8A245Leg1cq?si=fgGuy_HsTz6Q5FBx7WMZmw",
+  },
+  {
+    month: "September",
+    cover: "2017-09.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/2pUNwR8VxgHqsBluFIPExD?si=Suu9l-QIQ-iuItW8YXgWTw",
+  },
+  {
+    month: "October",
+    cover: "2017-10.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/5c3KA47JvDroowcHkwQ9Iq?si=zFi_FsgMQ5K_yFh2d8wAaw",
+  },
+  {
+    month: "November",
+    cover: "2017-11.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/1Do0hDVPpjKJDlH5hPeUxt?si=xfB2X2h8RuaF0-0XE3y7Dg",
+  },
+  {
+    month: "December",
+    cover: "2017-12.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/1stOSXFYiSZ8KDEnkwSvjS?si=dUY-IB5hR62Ilu0LSJqXxw",
+  },
+  {
+    month: "2017",
+    cover: "2017.jpg",
+    url: "https://open.spotify.com/user/daneden/playlist/0H82mPShpI8BpboBkjXwfI?si=tNPr7BNnRHmr8QD8kZlsfw",
+  },
+]
+
+export default months
